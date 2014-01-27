5e1b765e585ce3960d47b20833e1d36fca0039a6
Dawnson <shockwav213@hotmail.com>
2014-01-27 07:34:20 -0800

    conflict 2

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -78,29 +78,8 @@ public class GitLab implements EntryPoint {
 	public void displayUsers(List<IUser> users)
 	{
 
-		RootPanel.get("root").add(flexTable);
-		
-		flexTable.setText(0,0, "Name");
-		
-		flexTable.setText(0,1,"Language");
-		flexTable.setText(0,2, "Shopping Cart Size");
-		flexTable.setText(0,3, "Wish List Size");
-		flexTable.setStyleName("centered-table", true);
-		
-		for(int i=0; i < users.size(); i++)
-		{
-		
-			IUser user = users.get(i);
-			
-			flexTable.setText(i+1,0,user.getName());
-			if(user.getLanguage().trim().equals("EN"))
-		conflict 1
-			}
-			
-			flexTable.setText(i+1,2,String.valueOf(user.getShoppingCart().size()));
-			
-			flexTable.setText(i+1,3,String.valueOf(user.getWishList().size()));
-		}
-
+		;
+//conflict
 	}
+//	conflict
 }
