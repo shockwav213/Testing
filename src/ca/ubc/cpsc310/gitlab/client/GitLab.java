1e06e68b9243f51fc8f908faad8866745b57ee72
Dawnson <shockwav213@hotmail.com>
2014-01-27 06:22:59 -0800

    copy and paste

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -62,9 +62,9 @@ public class GitLab implements EntryPoint {
 
 			@Override
 			public void onSuccess(List<IUser> result) {
-				Window.alert("Got list back with " +  result.size() + " entries");
-				
-			}});
+				displayUsers(result);
+				}});
+
 	
 
 
